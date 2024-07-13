e2c51d5a183972aee35304c62f98d76fc6ce511f
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-07-13 18:42:09 +0000

    day 01: Add examples of variable declaration and assignment

---
+++ Day_01/day-01.js
@@ -0,0 +1,41 @@
+// Activity1
+var number = 10;
+console.log("var example", number)
+
+// Activity2
+let name = "John";
+console.log("let example", name)
+
+// Activity3
+const booleanValue = true;
+console.log("const example", booleanValue)
+
+
+//Activity4
+const integer = 10
+const decimal = 2.4
+const booleanVal = true
+const string = "String"
+const object = {key1: "value1", key2: "value2"}
+const array = ["one", "two"]
+const bigInteger = 1234567890123456789012345678901234567890n
+
+console.log("integer", typeof integer, "->", integer)
+console.log("integer", typeof decimal, "->", decimal)
+console.log("integer", typeof string, "->", string)
+console.log("integer", typeof booleanVal, "->", booleanVal)
+console.log("integer", typeof object, "->", object)
+console.log("integer", typeof array, "->", array)
+console.log("integer", typeof bigInteger, "->", bigInteger)
+
+// Activity5
+let num = 4
+console.log(num)
+num = 16;
+console.log(num)
+
+//Activity6
+const constVal = "const"
+// constVal = "can not be modified" // error
+
+
